cecf88896d87cf57b543a606ea90b3cd55583a30
Eugene <eugene.volovich@gmail.com>
2024-11-26 18:39:23 +0300

    First version of the website

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello, world!")
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route("/favicon.png")
+def get_image():
+    return send_file("files/favicon.png", mimetype="image/png")
+
+@app.route("/")
+def home():
+    return send_file("files/mainpage.html", mimetype="html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
